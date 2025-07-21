7feb4e86fd6444e47e39502181e0f1d29d3998c8
Lapidis2 <hitlapidis2@gmail.com>
2025-07-21 19:40:26 +0200

    using Link from next/Link

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,12 +1,20 @@
+import Link from 'next/link';
 export default function Header(){
 	return(
 		<header>
 			<h1>Welcome to My Project</h1>
 			<nav>
 				<ul>
-					<li><a href="/home">Home</a></li>
-					<li><a href="/about">About</a></li>
-					<li><a href="/contact">Contact</a></li>
+					<li>
+						<Link href="/">Home</Link>
+					</li>
+					<li>
+						<Link href="/about">About</Link>
+					</li>
+					<li>
+						<Link href="/contact">Contact</Link>	
+					</li>
+
 				</ul>
 			</nav>
 		</header>
