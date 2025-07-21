088adbedce44d8235bc1ba6e8e68f722103d09ff
Lapidis2 <hitlapidis2@gmail.com>
2025-07-21 23:29:32 +0200

    Use getStaticProps for Data Fetching

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '@/components/layout/Header';
 import PostCard from '@/components/common/PostCard';
 import { PostProps } from '../interfaces';
 
-const Posts = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
-      const data: PostProps[] = await response.json();
-      setPosts(data);
-    };
-
-    fetchPosts();
-  }, []);
-
+const Posts = ({ posts }: PostsPageProps) => {
   return (
     <>
       <Header />
@@ -35,6 +27,17 @@ const Posts = () => {
       </main>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+  const posts: PostProps[] = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Posts;
