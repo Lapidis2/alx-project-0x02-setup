1621991f7617a6551a553a48162f44e71c93cc6c
Lapidis2 <hitlapidis2@gmail.com>
2025-07-21 20:13:40 +0200

    updating interface

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { cardProps } from "../../interfaces";
+import { CardProps } from "../../interfaces";
 
-const Card: React.FC<cardProps> = ({ title, content }) => {
+const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
     <div className=" bg-blue-500 p-4 rounded-lg shadow-md text-2xl">
       <h2 className="card-title">{title}</h2>
--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,4 +1,4 @@
-export interface cardProps{
+export interface CardProps{
 	  title: string;
 	  content: string;
 }
