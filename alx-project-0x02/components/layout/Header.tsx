f2d3549e452f6ec093de96d9215587a4e2c5d9f2
Lapidis2 <hitlapidis2@gmail.com>
2025-07-21 19:35:13 +0200

    added routes for about and home pages

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -4,7 +4,7 @@ export default function Header(){
 			<h1>Welcome to My Project</h1>
 			<nav>
 				<ul>
-					<li><a href="/">Home</a></li>
+					<li><a href="/home">Home</a></li>
 					<li><a href="/about">About</a></li>
 					<li><a href="/contact">Contact</a></li>
 				</ul>
---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+	return (
+		<div>
+			<h1>About Page</h1>
+			<p>This page contains information about my project.</p>
+		</div>
+	);
+}
